4991ec02aea717d7d1f1b7e7d5ea5a3666d02f69
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2024-02-01 00:30:53 -0300

    Ok

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 <!doctype html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 	<link rel="stylesheet" href="<?=BASE_URL?>assets/css/styles.css?<?=TIME_UPDATE_CSS?>"/>
 </head>
